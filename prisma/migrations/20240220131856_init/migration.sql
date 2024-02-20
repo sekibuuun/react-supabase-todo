c22dd240420f8aed7759494a8512ab7049cf9059
Aoyama Kazuki <sekibun3109@gmail.com>
2024-02-20 22:21:06 +0900

    fix: スキーマ設計

---
+++ prisma/migrations/20240220131856_init/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `authorId` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `content` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `published` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `priority` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "authorId",
+DROP COLUMN "content",
+DROP COLUMN "published",
+ADD COLUMN     "Flag" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "description" TEXT,
+ADD COLUMN     "priority" INTEGER NOT NULL;
+
+-- DropTable
+DROP TABLE "User";
